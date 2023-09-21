a49647a212464b260ff6b8eb36ca09aa4842d7f5
Matthew Douglas <matthew@bytesizecoder.ca>
2023-09-21 13:44:05 -0400

    Add server authentication and authorization JWT

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using API.Data;
+using API.DTO;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService) 
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("register")] // api/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest("Username is taken");
+        }
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")] // api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var fetchedUser = await _context.Users.SingleOrDefaultAsync( user => user.UserName == loginDto.Username);
+
+        if (fetchedUser == null)
+        {
+            return Unauthorized("invaid credentials - username");
+        }
+
+        using var hmac = new HMACSHA512(fetchedUser.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; ++i) 
+        {
+            if (computedHash[i] != fetchedUser.PasswordHash[i])
+            {
+                return Unauthorized("invaid credentials - password");
+            }
+        }
+
+        return new UserDto
+        {
+            Username = fetchedUser.UserName,
+            Token = _tokenService.CreateToken(fetchedUser)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,12 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -26,11 +19,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors(corsBuilder => {
     corsBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
